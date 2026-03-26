ee25d3593ae01b5cf3ccd7c6a69d8812332b5608
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-03-26 10:33:20 +0530

    Update for CF

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare';
 import sitemap from '@astrojs/sitemap';
 import pagefind from 'astro-pagefind';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://ajay.social',
-  base: '/sirconandoyle/',
+  site: 'https://sirconandoyle.com',
   output: 'static',
-  // adapter: cloudflare(),
+  adapter: cloudflare(),
 
   integrations: [
     sitemap(),
